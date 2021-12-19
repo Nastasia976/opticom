b8faae829915c85db5d5d7673253a33d722e7234
Nastasia976 <chontybaeva@list.ru>
2021-12-19 15:57:58 +0300

    sfv,dflv,

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -57,27 +57,125 @@ $(document).ready(function () {
         $(this).removeClass('active').prev().css("opacity", "1");
     }); */
 
-    
-    $('.location').hover(
-        function(){
+
+    $('.small-location').hover(
+        function () {
             $('.location').css("opacity", "1");
             $(this).css("opacity", "0");
             $('.location-main').css("opacity", "1");
             $('.popup__title').html($(this).attr('location-data'));
             $('.popup').show();
         },
-        function() {
-            $('.location').css("opacity", "0");
+        function () {
+        }
+    );
+
+    $('.location-main').hover(
+        function () {
+            $(this).css("opacity", "1");
+            $('.location').css("opacity", "1");
             $('.popup').hide();
-            
+        },
+        function () {
+            $(this).css("opacity", "0");
+            $('.location').css("opacity", "0");
         }
     );
 
 });
+$('.school').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '150px',
+            'left': '550px'
+        });
+    }
+});
+$('.hospital').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '40px',
+            'left': '565px'
+        });
+    }
+});
+$('.azs').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '60px',
+            'left': '250px'
+        });
+    }
+});
+$('.autoservise').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '240px',
+            'left': '305px'
+        });
+    }
+});
+$('.hotel').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '175px',
+            'left': '280px'
+        });
+    }
+});
+$('.restaurant').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '190px',
+            'left': '325px'
+        });
+    }
+});
+$('.shop').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '270px',
+            'left': '440px'
+        });
+    }
+});
+$('.office').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '320px',
+            'left': '530px'
+        });
+    }
+});
+$('.bakery').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '380px',
+            'left': '600px'
+        });
+    }
+});
+$('.delivery').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '250px',
+            'left': '420px'
+        });
+    }
+});
+$('.fabrica').on({
+    mouseenter: function () {
+        $('.popup').css({
+            'top': '180px',
+            'left': '500px'
+        });
+    }
+});
 
 
 
-    
+
+
 
 
 
