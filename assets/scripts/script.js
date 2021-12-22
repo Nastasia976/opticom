ce315f8df6cb6b2b1433d94c59ad5acf6c63d3a9
Nastasia976 <chontybaeva@list.ru>
2021-12-22 14:22:40 +0300

    l;,l,slmvkl

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -213,6 +213,9 @@ new Swiper('.swiper-progress', {
     }
 });
 
+
+
+
 $("body").on('click', '[href*="#"]', function(e){
     var fixed_offset = 0;
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
